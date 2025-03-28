05f2c7e3c02e78b0ba2d9d383c42f55ce4bfb29a
borjaocerin <115790090+borjaocerin@users.noreply.github.com>
2025-03-28 20:10:21 +0100

    da

--- api-gateway/gateway.js
+++ api-gateway/gateway.js
@@ -58,7 +58,7 @@ app.get('/api/users/auth0-login', (req, res) => {
     console.log('Solicitud de login con Auth0 recibida');
     
     // Redirigir al microservicio que maneja Auth0 
-    res.redirect('http://172.26.6.222:5000/auth0-login'); 
+    res.redirect('http://microservicio_usuarios:5000/auth0-login'); 
 });
 
 
